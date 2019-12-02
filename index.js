cd07c85d97f74111a2937b108e84c8e47d3c72ba
裴谦 <159071@nd.com>
2019-12-02 12:03:27 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,95 @@
+const fs = require("fs");
+const path = require("path");
+const utils = require("./utils");
+const hookList = require("./hookList");
+
+const gitRevParse = utils.gitRevParse;
+const isHusky = utils.isHusky;
+
+const { gitCommonDir } = gitRevParse();
+
+function getHooks(gitDir) {
+  const gitHooksDir = path.join(gitDir, "hooks");
+  return hookList.map(hookName => path.join(gitHooksDir, hookName));
+}
+
+function getTargetHookPath(gitDir, name) {
+  return path.join(gitDir, "hooks", name);
+}
+
+function isHuskyFile(filename) {
+  if (fs.existsSync(filename)) {
+    const data = fs.readFileSync(filename, "utf-8");
+    return isHusky(data);
+  }
+
+  return false;
+}
+
+function fix(target, append) {
+  console.log("begin to fix: ", target, append);
+  const huskyHookFiles = getHooks(gitCommonDir).filter(hook =>
+    isHuskyFile(hook)
+  );
+  const targetHookFilePath = getTargetHookPath(gitCommonDir, target);
+  let originTargetHookScript;
+  if (fs.existsSync(targetHookFilePath) && !isHuskyFile(targetHookFilePath)) {
+    originTargetHookScript = fs.readFileSync(targetHookFilePath, "utf-8");
+  }
+
+  if (huskyHookFiles && huskyHookFiles.length && originTargetHookScript) {
+    const huskyScript = fs.readFileSync(huskyHookFiles[0], "utf-8");
+    let mergedHookScript = "";
+    if (append) {
+      mergedHookScript = `${originTargetHookScript}
+
+      # Merged at ${new Date().toLocaleString()}
+
+      ${huskyScript}
+
+      `;
+    } else {
+      mergedHookScript = `${huskyScript}
+
+      # Merged at ${new Date().toLocaleString()}
+
+      ${originTargetHookScript}
+
+      `;
+    }
+    // console.log(mergedHookScript)
+    fs.writeFileSync(targetHookFilePath, mergedHookScript, "utf-8");
+    fs.chmodSync(targetHookFilePath, 0o0755);
+    console.log("husky-merge merge success: ", target, append);
+  } else {
+    console.log("husky-merge huskyHookFiles: ", huskyHookFiles);
+    console.log("husky-merge originTargetHookScript: ", originTargetHookScript);
+    console.log("husky-merge merge failed");
+  }
+}
+
+try {
+  const program = require("commander");
+
+  program
+    .version(require("./package.json").version)
+    .option("-t, --target <fileName>", "target hook file name", "commit-msg")
+    .option(
+      "-a, --append",
+      "append husky script to the end of exited hook file(default husky is in the front)"
+    )
+    .option("-l, --list ", "list all husky hook file name")
+    .parse(process.argv);
+
+  if (program.list) {
+    console.log("All hookList is");
+    console.log("=======");
+    hookList.forEach(item => console.log(item));
+    console.log("=======");
+  } else if (program.target) {
+    fix(program.target, program.append);
+  }
+
+} catch (err) {
+  console.log("husky-merge merge error", err);
+}
---
+++ utils.js
@@ -0,0 +1,45 @@
+// husky/lib/installer/gitRevParse @3.1.0
+const slash = require("slash");
+const execa = require("execa");
+
+function gitRevParse() {
+  // https://github.com/typicode/husky/issues/580
+  // https://github.com/typicode/husky/issues/587
+  const result = execa.sync("git", [
+    "rev-parse",
+    "--show-toplevel",
+    "--git-common-dir"
+  ]);
+
+  const [topLevel, gitCommonDir] = result.stdout
+    .trim()
+    .split("\n")
+    // Normalize for Windows
+    .map(slash);
+
+  // Git rev-parse returns unknown options as is.
+  // If we get --absolute-git-dir in the output,
+  // it probably means that an older version of Git has been used.
+  if (gitCommonDir === "--git-common-dir") {
+    throw new Error("Husky requires Git >= 2.13.0, please upgrade Git");
+  }
+
+  return { topLevel, gitCommonDir };
+}
+
+// husky/lib/installer/is @3.1.0
+function isHusky(data) {
+  // Husky v0.14 and prior used #husky as an identifier.
+  // Just in case some previous hooks weren't correctly uninstalled,
+  // and for a better transition this will allow v0.15+ to uninstall them as well.
+  const previousHuskyIdentifier = "#husky";
+  return (
+    data.indexOf(huskyIdentifier) !== -1 ||
+    data.indexOf(previousHuskyIdentifier) !== -1
+  );
+}
+
+module.exports = {
+  gitRevParse,
+  isHusky
+};
